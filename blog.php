7e82593c809fc4f691a704be56ec8506f10a619e
cwillaim <cwwomack@mit.edu>
2016-04-23 23:38:54 -0400

    use https for material icons import

--- blog.php
+++ blog.php
@@ -11,7 +11,7 @@
     <link href="stylesheets/blog.css" rel="stylesheet">
     <link href="stylesheets/main.css" rel="stylesheet">
     <!--Import Google Icon Font-->
-    <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 
     <script src="js/jquery.js"></script>
     <script type='text/javascript' src='js/materialize.min.js'></script>
@@ -25,15 +25,15 @@
     <!-- Header -->
     <nav class="z-depth-0">
       <div class="nav-wrapper">
-        <a href="/club/" class="brand-logo"><img class="logo hide-on-med-and-down" src="images/logo-white.png"></a>
+        <a href="/" class="brand-logo"><img class="logo hide-on-med-and-down" src="images/logo-white.png"></a>
         <a href="#" data-activates="mobile-nav" class="button-collapse"><i class="material-icons">menu</i></a>
         <ul class="right hide-on-med-and-down">
-          <li><a href="/club/">HOME</a></li>
+          <li><a href="/">HOME</a></li>
           <li><a href="blog.php">BLOG</a></li>
           <li><a href="programs.html">PROGRAMS</a></li>
         </ul>
         <ul class="side-nav" id="mobile-nav">
-          <li><a href="/club/">HOME</a></li>
+          <li><a href="/">HOME</a></li>
           <li><a href="blog.php">BLOG</a></li>
           <li><a href="programs.html">PROGRAMS</a></li>
         </ul>
