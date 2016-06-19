0d15f936551df0c62f89c120c04ce88da5898dc3
cwillaim <cwwomack@mit.edu>
2016-06-18 22:00:44 -0400

    rearrange blog preview on homepage; set font sizes for posts

--- js/ieee.js
+++ js/ieee.js
@@ -1,22 +1,9 @@
-// This code was used for forcibly overriding CuteNews styles
-// it may not be necessary anymore
-
 $(document).ready(function() {
 
+    // Initialize side nav
     $('.button-collapse').sideNav();
 
-    //Get rid of styling from CuteNews
-    $('div b', '.post').contents().unwrap();
-    $('div font', '.post').contents().unwrap();
-    $('div[style]', '.post').removeAttr('style');
-    $('span b', '.post').contents().unwrap();
-    $('span font', '.post').contents().unwrap();
-    $('span[style]', '.post').removeAttr('style');
-    $('a[style]', '.post').removeAttr('style');
-    $('div', '.post').addClass('flow-text');
-    $('p', '.post').addClass('flow-text');
-
-    // We probably still need this part though
+    // Add in image styling
     $('p img', '.post').addClass('center-block z-depth-1');
     $('p img', '.post').unwrap().wrap("<div class='blog-image-padding' />");
 
